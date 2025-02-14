5274cdb1e0273469b537bb8491c6f256e539e05b
espinozagabriel0 <bascopeespinozagabriel@fpllefia.com>
2025-02-14 19:00:50 +0100

    inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Calendar } from './components/ui/calendar'
+
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl border text-center'>Proyecto</h1>
+      <Calendar/>
+    </>
+  )
+}
+
+export default App
